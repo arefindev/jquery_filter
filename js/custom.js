937a0c80aebe84904fd391a5030984eac1bb0508
Morshedul Arefin <arefin2k@gmail.com>
2022-09-17 06:16:02 +0600

    working

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,10 @@
 (function($){
-
     "use strict";
-
-    $('.filtr-container').filterizr({
-        animationDuration: 0.5,
-        callbacks: {
-            onInit: function() { },
-            onFilteringStart: function() { },
-            onFilteringEnd: function() { },
-            onShufflingStart: function() { },
-            onShufflingEnd: function() { },
-            onSortingStart: function() { },
-            onSortingEnd: function() { }
-        },
-        controlsSelector: '',
-        delay: 0,
-        delayMode: 'progressive',
-        easing: 'ease-out',
-        filter: 'all',
-        filterOutCss: {
-            opacity: 0,
-            transform: 'scale(0.5)'
-        },
-        filterInCss: {
-            opacity: 0,
-            transform: 'scale(1)'
-        },
-        gridItemsSelector: '.filtr-item',
-        gutterPixels: 0,
-        layout: 'sameSize',
-        multifilterLogicalOperator: 'or',
-        searchTerm: '',
-        setupControls: true,
-        spinner: {
-            enabled: false,
-            fillColor: '#2184D0',
-            styles: {
-            height: '75px',
-            margin: '0 auto',
-            width: '75px',
-            'z-index': 2,
-            },
-        }
+    var $grid = $('.filter-items').isotope();
+    $('.filter ul').on( 'click', 'li', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+        $(this).siblings(".active").removeClass('active');
+        $(this).addClass("active");
     });
-
 })(jQuery);
